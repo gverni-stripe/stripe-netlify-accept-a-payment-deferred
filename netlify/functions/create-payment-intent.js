c1c9dfdc38c412b7802e02575798cfc27444b370
Giuseppe Verni <gverni@stripe.com>
2024-01-09 17:45:23 +0000

    Add handling of Payment Method Type
    Depending on which elment is creating the payment intent, use
    different payment method type
    (This is only for illustration pourpose)

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,20 +1,31 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY, {
-    appInfo: { // For sample support and debugging, not required for production:
-      name: "stripe-samples/accept-a-payment-deferred/payment-element",
-    }
-  });
+  appInfo: {
+    // For sample support and debugging, not required for production:
+    name: 'stripe-samples/accept-a-payment-deferred/payment-element',
+  },
+});
 
-exports.handler = async () => {
+exports.handler = async (request) => {
   // Create a PaymentIntent with the amount, currency, and a payment method type.
   //
   // See the documentation [0] for the full list of supported parameters.
   //
   // [0] https://stripe.com/docs/api/payment_intents/create
+
+  const paymentMethodOptions =
+    request.queryStringParameters.payment_method_types 
+      ? {
+          payment_method_types: ['card', 'link'],
+        }
+      : {
+          automatic_payment_methods: {enabled: true},
+        };
+
   try {
     const paymentIntent = await stripe.paymentIntents.create({
       currency: process.env.CURRENCY || 'usd',
       amount: 1999,
-      automatic_payment_methods: {enabled: true},
+      ...paymentMethodOptions,
     });
 
     // Send publishable key and PaymentIntent details to client
